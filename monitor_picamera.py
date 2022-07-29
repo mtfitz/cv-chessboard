49ed93320e2569a636134798bfd4cd561a39a569
Michael Fitzgerald <mtf323@lehigh.edu>
2022-07-29 15:20:07 -0400

    FPS counter for debugging

--- monitor_picamera.py
+++ monitor_picamera.py
@@ -3,15 +3,23 @@ from picamera import PiCamera
 import time
 import cv2
 
+i = 0
 with PiCamera() as camera:
+    camera.resolution = (480,360)
+    camera.framerate = 15
     stream = PiRGBArray(camera)
-    camera.resolution = (640,480)
-    camera.framerate = 30
+    ts = time.time()
     for frame in camera.capture_continuous(stream, format='bgr', use_video_port=True):
         image = frame.array
         cv2.imshow('Monitor', image)
+        i += 1
+        #print(time.time())
         if cv2.waitKey(33) == 27:
             break
         
         stream.truncate()
         stream.seek(0)
+        
+    te = time.time()
+    fps = i / (te - ts)
+    print(fps)
